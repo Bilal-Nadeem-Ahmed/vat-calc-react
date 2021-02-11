b46729a893c03ce71c5f5875b9182eaa69cc6a02
Bilal Ahmed <bilal91_@hotmail.com>
2021-02-11 02:14:24 +0000

    vatonpurch second div now works
    look at the calcbutton or make a new one for purchases as its so different

--- my-app/src/SalesAndPurchasesCalculator/TableRows.js
+++ my-app/src/SalesAndPurchasesCalculator/TableRows.js
@@ -1,28 +1,48 @@
 import {useState} from 'react'
-    const TableRows = ({desc,word}) => {
-    const [val,setVal]=useState(0)
   
-  const handleInvalid=(e)=>{
+const TableRows = ({desc,word,obj,addtoarr}) => {
+  const [val,setVal]=useState(0)
+  const [vatVal,setVatVal]=useState(0)
+  
+  const handleClick=(e)=>{
+
     e.target.value=''
-      setTimeout(()=>{
-        if(e.target.value===''){
-    
-            e.target.value=0;
-    
-      }else{
-    
-        console.log('ay')
-    
-    } },3000)
+
+    setTimeout(()=>{
+
+      e.target.value==='' ? e.target.value=0:console.log('no')
+
+    },2500)
      
   }
 
+  const handleChange=(e)=>{
+    setVal(e.target.value)
+    console.log('ay')
+  }
+
+  const handleChangeVat=(e)=>{
+      setVatVal(e.target.value)
+  }
+
+  const HandleChangeVatVal=(e)=>{
+
+
+   return                 <input  className={`${desc}vr-input`} min={1} onChange={
+    (e)=>{ addtoarr(e.target.val);console.log(obj)}
+   }  
+   
+   defaultValue={vatVal/120*20}
+    onClick={(e)=>handleClick(e)} type="number"/>
+ 
+  //  vatVal/120*20
+  }
      
    if(word=='sales'){
     return ( 
         <tr>  
             <td>
-                <input  className={`${desc}-input`} min={1}   defaultValue={0} onClick={(e)=>handleInvalid(e)} type="number"/>
+                <input  className={`${desc}-input`} min={1}  onChange={(e)=>handleChange(e)} defaultValue={0} onClick={(e)=>handleClick(e)} type="number"/>
             </td>
             <td>{val/120 *20}</td>
         </tr>
@@ -32,9 +52,13 @@ import {useState} from 'react'
        return(
        <tr>  
             <td>
-                <input  className={`${desc}-input`} min={1}   defaultValue={0} onClick={(e)=>handleInvalid(e)} type="number"/>
+                <input  className={`${desc}v-input`} min={1} onChange={(e)=>handleChangeVat(e)}  defaultValue={0} onClick={(e)=>handleClick(e)} type="number"/>
             </td>
-            <td>{val/120 *20}</td>
+            <td>
+                <HandleChangeVatVal/>
+                  
+            </td>
+            
         </tr>
        )
      
--- my-app/src/SalesAndPurchasesCalculator/VatMonth.js
+++ my-app/src/SalesAndPurchasesCalculator/VatMonth.js
@@ -1,12 +1,16 @@
-
-import TableRows from './TableRows'
+import {useState} from 'react';
+import TableRows from './TableRows';
 import TableRowsPurchases from './TableRows'
-
 
 
 const VatOnSalesMonth = ({desc,month,word}) => {
   
-   
+  //temp for vat on purch component  
+const  obj=[]
+const addToArray=(a)=>{
+  obj.push(a)
+
+}
  
   const days=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
   
@@ -24,7 +28,7 @@ const VatOnSalesMonth = ({desc,month,word}) => {
 
                   { days.map(i=>{
                         
-                    return <TableRows word={word} desc={desc} key={`${desc}-${i}`}  />
+                    return <TableRows  addtoarr={(x)=>addToArray(x)}obj={obj} word={word} desc={desc} key={`${desc}-${i}`}  />
                   
                   })}
 
